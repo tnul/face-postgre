29a2f42d3e5a7f7c70ed6622bd4f415236efc6d6
d-demirci <ddemirci@windowslive.com>
2018-06-10 00:40:18 +0300

    edited db.py

--- db.py
+++ db.py
@@ -4,7 +4,7 @@ import psycopg2
 def setup_db():
     dbconnection = psycopg2.connect("user='jfaceprojectuser' password='jfaceprojectpassword' host='172.17.0.2' dbname='jfaceprojectdb'")
     db=dbconnection.cursor()
-    #db.execute("create extension if not exists cube;")
+    #db.execute("create extension if not exists cube;") this should be run as postgres user 
     db.execute("drop table if exists vectors")
     db.execute("create table vectors (id serial, file varchar, vec_low cube, vec_high cube);")
     db.execute("create index vectors_vec_idx on vectors (vec_low, vec_high);")
